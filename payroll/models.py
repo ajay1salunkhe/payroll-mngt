14caa57846ffeffac1b76345ce9198a8e3e6724d
ajay1salunkhe <mail2ajaysalunkhe123@gmail.com>
2018-03-01 16:26:55 +0530

    models.py updated -- model Employee created

--- payroll/models.py
+++ payroll/models.py
@@ -6,6 +6,19 @@ class Employee(models.Model):
     emp_id = models.AutoField('auth.User', primary_key=True)
     emp_name = models.CharField(max_length=30)
     emp_address = models.TextField(max_length=50)
+    emp_contact = models.CharField(max_length=15)
+    emp_alt_contact = models.CharField(max_length=15)
+    emp_email = models.EmailField()
+    gender_choices = (('M','Male'),('F','Female'))
+    emp_gender = models.CharField(max_length=7,choices=gender_choices)
+    emp_dob = models.dateTimeField()
+    emp_pan_id = models.models.CharField(max_length=15)
+    emp_aadhar_no = models.models.CharField(max_length=15)
+    emp_profile_pic = models.models.BinaryField()
+    tax_status = ((NRI,NRI),(RESIDENT,RESIDENT),(EXPAT,EXPAT))
+    emp_tax_status = models.CharField(max_length=10,choices=tax_status)
+    emp_total_leave = models.PositiveSmallIntegerField() 
+
     def publish(self):
         self.save()
 
