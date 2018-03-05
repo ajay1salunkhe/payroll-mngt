94d7d3e879d3360dd705e8ab8266c9c738ad7cb1
ajay1salunkhe <mail2ajaysalunkhe123@gmail.com>
2018-03-05 12:52:04 +0530

    .gitignore modified

--- payroll/models.py
+++ payroll/models.py
@@ -3,21 +3,21 @@ from django.utils import timezone
 
 
 class Employee(models.Model):
-    emp_id = models.AutoField('auth.User', primary_key=True)
-    emp_name = models.CharField(max_length=30)
-    emp_address = models.TextField(max_length=50)
-    emp_contact = models.CharField(max_length=15)
-    emp_alt_contact = models.CharField(max_length=15)
-    emp_email = models.EmailField()
-    gender_choices = (('M','Male'),('F','Female'))
-    emp_gender = models.CharField(max_length=7,choices=gender_choices)
-    emp_dob = models.DateField()
-    emp_pan_id = models.CharField(max_length=15)
-    emp_aadhar_no = models.CharField(max_length=15)
-    emp_profile_pic = models.BinaryField()
-    tax_status = (("nri","NRI"),("resident","RESIDENT"),("expat","EXPAT"))
-    emp_tax_status = models.CharField(max_length=10,choices=tax_status)
-    emp_total_leave = models.PositiveSmallIntegerField()
+#    emp_id = models.AutoField('auth.User', primary_key=True)
+    name = models.CharField(max_length=30)
+    address = models.TextField(max_length=50)
+    contact = models.CharField(max_length=15)
+    alt_contact = models.CharField(max_length=15)
+    email = models.EmailField()
+    genderchoices = (('M','Male'),('F','Female'))
+    gender = models.CharField(max_length=7,choices=gender_choices)
+    dob = models.DateField()
+    pan_id = models.CharField(max_length=15)
+    aadhar_no = models.CharField(max_length=15)
+    profile_pic = models.BinaryField()
+    tax_status_choices = (("nri","NRI"),("resident","RESIDENT"),("expat","EXPAT"))
+    tax_status = models.CharField(max_length=10,choices=tax_status_choices)
+    total_leave = models.PositiveSmallIntegerField()
 
     def publish(self):
         self.save()
@@ -26,9 +26,10 @@ class Employee(models.Model):
         return self.emp_name
 
 class Attendance(models.Model):
-    att_date = models.DateField()
+    employee = models.ForeignKey(Employee)
+    date = models.DateField()
     mark_choices = ((0,'Absent'),(0.5,'Half day'),(1,'Present'))
-    att_mark = models.IntegerField(max_length=3,choices=mark_choices)
+    mark = models.IntegerField(max_length=3,choices=mark_choices)
     is_half_day = False
     def publish(self):
         if mark_choices==0:
@@ -39,3 +40,5 @@ class Attendance(models.Model):
             leave_type="Half Day"
             is_half_day=True
         self.save()
+
+#class Job
