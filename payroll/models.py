afca2a6e1f07b061c66f83c11d3b2dcbfcdb6cc5
ajay1salunkhe <mail2ajaysalunkhe123@gmail.com>
2018-03-05 17:50:17 +0530

    models.py changes

--- payroll/models.py
+++ payroll/models.py
@@ -3,34 +3,35 @@ from django.utils import timezone
 
 
 class Employee(models.Model):
-#    emp_id = models.AutoField('auth.User', primary_key=True)
     name = models.CharField(max_length=30)
-    address = models.TextField(max_length=50)
+    address = models.TextField(max_length=50,null=True)
     contact = models.CharField(max_length=15)
-    alt_contact = models.CharField(max_length=15)
+    alt_contact = models.CharField(max_length=15,null=True)
     email = models.EmailField()
     gender_choices = (('M','Male'),('F','Female'))
     gender = models.CharField(max_length=2,choices=gender_choices)
     dob = models.DateField()
-    pan_id = models.CharField(max_length=15)
+    pan_id = models.CharField(max_length=15,null=True)
     aadhar_no = models.CharField(max_length=15)
     profile_pic = models.BinaryField()
     tax_status_choices = (("nri","NRI"),("resident","RESIDENT"),("expat","EXPAT"))
     tax_status = models.CharField(max_length=10,choices=tax_status_choices)
-    total_leave = models.PositiveSmallIntegerField()
+    total_leave = models.PositiveSmallIntegerField(default=0)
 
     def publish(self):
         self.save()
 
     def __str__(self):
-        return self.emp_name
+        return self.name
 
 class Attendance(models.Model):
     employee = models.ForeignKey(Employee)
-    date = models.DateField()
+    date = models.DateField(null=True)
     mark_choices = ((0,'Absent'),(0.5,'Half day'),(1,'Present'))
     mark = models.IntegerField(choices=mark_choices)
     is_half_day = False
+
+
     def publish(self):
         if mark_choices==0:
             leave_type="Absent"
