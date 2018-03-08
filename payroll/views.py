8fbbf2d75fccfe219110305f1014f0b1611c81a3
ajay1salunkhe <mail2ajaysalunkhe123@gmail.com>
2018-03-08 18:06:55 +0530

    views and templates added

--- payroll/views.py
+++ payroll/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Employee
+from django.utils import timezone
 
 # Create your views here.
+
+def post_list(request):
+    employees = Employee.objects.filter(joining_date__lte=timezone.now()).order_by('joining_date')
+    return render(request,'payroll/post_list.html',{'employees':employees})
